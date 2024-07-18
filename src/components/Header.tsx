a27347af14d8ed7c5861e054c63bf6457bab501e
Kacperkg <gajdarski.kacper@gmail.com>
2024-07-19 00:02:09 +0100

    changes to package.json

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,8 @@ export default function Header() {
     return (
         <>
             <section className={HeaderCSS.header}>
+                <div className="header-blur-1"></div>
+                <div className="header-blur-2"></div>
                 <h1 className={HeaderCSS.heading}>
                     Partnerships that encourage your passion.
                 </h1>
